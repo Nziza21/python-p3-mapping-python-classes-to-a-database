ca81c50a60a3faf2292def9e11f09ce398686272
Nziza Samuel <nziza.samuel@student.moringaschool.com>
2023-12-05 21:29:01 +0300

    completed

--- lib/song.py
+++ lib/song.py
@@ -1,4 +1,31 @@
-from config import CONN, CURSOR
+import sqlite3
 
 class Song:
-    pass
+    def __init__(self, name, album, song_id=None):
+        self.id = song_id  
+        self.name = name
+        self.album = album
+
+    def save(self):
+        with sqlite3.connect('music.db') as conn:
+            cursor = conn.cursor()
+            if self.id is None:
+                cursor.execute("INSERT INTO songs (name, album) VALUES (?, ?)", (self.name, self.album))
+                self.id = cursor.lastrowid
+            else:
+                cursor.execute("UPDATE songs SET name=?, album=? WHERE id=?", (self.name, self.album, self.id))
+
+    @classmethod
+    def create_table(cls):
+        with sqlite3.connect('music.db') as conn:
+            conn.execute('''CREATE TABLE IF NOT EXISTS songs (
+                            id INTEGER PRIMARY KEY AUTOINCREMENT,
+                            name TEXT,
+                            album TEXT
+                         )''')
+
+    @classmethod
+    def create(cls, name, album):
+        song = cls(name, album)
+        song.save()
+        return song
--- lib/testing/class_test.py
+++ lib/testing/class_test.py
@@ -1,23 +1,26 @@
-from lib.config import CURSOR
+from lib.config import CONN, CURSOR
 from lib.song import Song
 
 class TestClass:
     '''Class Song in song.py'''
 
+    @classmethod
+    def setup_class(cls):
+        CURSOR.execute('''CREATE TABLE IF NOT EXISTS songs (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, album TEXT)''')
+        CONN.commit()
+
     def test_creates_songs_table(self):
         '''has classmethod "create_table()" that creates a table "songs" if table does not exist.'''
         Song.create_table()
-        assert(CURSOR.execute("SELECT * FROM songs"))
+        assert CURSOR.execute("SELECT * FROM songs")
 
     def test_initializes_with_name_and_album(self):
         '''takes a name and album as __init__ arguments and saves them as instance attributes.'''
         song = Song("Hold On", "Born to Sing")
-        assert(song.name == "Hold On" and song.album == "Born to Sing")
+        assert song.name == "Hold On" and song.album == "Born to Sing"
 
     def test_saves_song_to_table(self):
         '''has instancemethod "save()" that saves a song to music.db.'''
-
-        
         CURSOR.execute('''DROP TABLE IF EXISTS songs''')
         Song.create_table()
 
@@ -27,11 +30,10 @@ class TestClass:
             'SELECT * FROM songs WHERE name=? AND album=?',
             ('Hold On', 'Born to Sing')
         ).fetchone()
-        assert(db_song[1] == song.name and db_song[2] == song.album)
+        assert db_song[1] == song.name and db_song[2] == song.album
 
     def test_creates_and_returns_song(self):
         '''has classmethod "create()" that creates a Song instance, saves it, and returns it.'''
-
         CURSOR.execute('''DROP TABLE IF EXISTS songs''')
         Song.create_table()
 
@@ -41,4 +43,4 @@ class TestClass:
             ('Hold On', 'Born to Sing')
         ).fetchone()
 
-        assert(db_song[0] == song.id and db_song[1] == song.name and db_song[2] == song.album)
+        assert db_song[0] == song.id and db_song[1] == song.name and db_song[2] == song.album
